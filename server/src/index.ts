c03234be5a1674e08bd5370a000bb55bdd47223f
Aurelia Robu <robuaurelia@gmail.com>
2024-08-23 22:03:32 +0300

    Server setup

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import {config} from "dotenv";
+import Stripe from 'stripe';
+import {app} from "./api";
+
+if (process.env.NODE_ENV !== 'production'){
+    config();
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+const port = process.env.PORT || 3333;
+
+app. listen(port, ()=> {
+    console.log(`API available on http://localhost:${port}`)
+})
+
